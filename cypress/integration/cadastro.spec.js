4b4f83f1b9cfb34c14ea784a9f36c5f70c07e3b5
Matheus Marcena <matheus13_felipe@hotmail.com>
2022-05-19 21:39:58 -0300

    Adição teste negativo

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -63,5 +63,50 @@ describe('cadastro', ()=>{
        cy.get('#iptNumeroEmpresa').type(number)
        cy.get('div.button__container > button').click()
        cy.get('div > p.complete-registration__text.complete-registration__text--primary')
-    })
+    }),
+    it('Erro no preenchimento do cpf', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://sandbox.melhorenvio.com.br/cadastre-se')
+ 
+        cy.get('Form> div > h1').should('have.text', ' Cadastre-se ')
+        
+        var firstName = faker.name.firstName()
+        var lastName = faker.name.lastName()
+        var email = faker.internet.email()
+        let meuCpf = makeid(8)
+        var randomCnpj = faker_br.br.cnpj()
+        var phone = makeid(8)
+        var ddd = makeid(2)
+        var company = firstName + lastName + 'company'
+        var cep = '51190470'
+        var number = makeid(3)
+ 
+ 
+        var cliente ={
+            nome: firstName+' '+lastName,
+            email: email,
+            confirmaEmail: email,
+            cpf: meuCpf,
+            dataNascimento: '18121996',
+            senha: 'abc123',
+            confirmaSenha: 'abc123',
+            celular: ddd+'9'+phone,
+            
+        }
+ 
+        cy.get('#iptNome').type(cliente.nome)
+        cy.get('#iptEmail').type(cliente.email)
+        cy.get('.lead-form__actions>button.button.button--primary').trigger('mouseover').wait(1000).click({force:true})
+        
+        cy.xpath('//input[@id="iptCPF"]').type(cliente.cpf)
+        cy.get('#iptDtNascimento').type(cliente.dataNascimento)
+        cy.get('#iptCelular').type(cliente.celular)
+        cy.get('#iptConfirmaEmail').type(cliente.confirmaEmail)
+        cy.get('#iptSenha').type(cliente.senha)
+        cy.get('#iptConfirmaSenha').type(cliente.confirmaSenha)
+        cy.get('div:nth-child(6) > fieldset > div > label').click()
+        cy.get('button.button.button--primary').click().click({force:true})
+        cy.contains('p.errorText', 'CPF inválido! Verifique sua resposta.')
+        
+     })
 })
